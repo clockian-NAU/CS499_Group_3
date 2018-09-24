c242896c278e781f18c7a676ca09f8d608fc299d
YuxuanCSxingkong <36606851+xingkongzyx@users.noreply.github.com>
2018-09-23 17:05:13 -0700

    fininsh str_to_con.c

--- assignmentPR/src/string_to_int_converter.c
+++ assignmentPR/src/string_to_int_converter.c
@@ -18,9 +18,33 @@
 #ifndef STRING_TO_INT_CONVERTER_C
 #define STRING_TO_INT_CONVERTER_C
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h> 
+#include <string.h>
 #include "string_to_int_converter.h"
+
+/*
+ * @brief convert a number string to real integer
+ *
+ * @param str[] A string only combining with numbers 
+ *
+ * @return int The integer after converting will be returned back
+ */
+
+
+int convertStrToNum (char str[])
+{
+    int result = 0;
+    int len = strlen(str);
+    for(int i = 0; i < len; i++)
+    {
+        result = result * 10 + (str[i] - '0');
+    }
+    return result;
+}
+
+
 
 /*
  * @brief Finds, creates, and stores the characteristic
@@ -36,13 +60,72 @@
 bool characteristic(char numString[], int *c){
     // TODO: Check numstring for proper formatting
 
+    
     // TODO: Obtain string of the characteristic
-
+    int length = strlen(numString);
+    char *resultString;
+    resultString = (char*)malloc(length*sizeof(char));
+    bool flag1 = true;
+    bool flag2 = true;
+    int countFloat = 0;
+    for (int i = 0; i < length; ++i)
+    {
+        if (numString[i] == '.')
+        {
+            flag1 = false;
+            countFloat = i;
+        }
+        if (numString[i] == '-')
+        {
+            flag2 = false;
+        }
+    }
+    if(flag2 == false)
+    {
+        if (flag1 == false)
+        {
+            for (int i = 1; i < countFloat; ++i)
+            {
+                resultString[i-1] = numString[i];
+            }
+        }
+        else
+        {
+            for (int i = 1; i < length; ++i)
+            {
+                resultString[i-1] = numString[i];
+            }
+        }
+    }
+    else
+    {
+        if (flag1 == false)
+        {
+            for (int i = 0; i < countFloat; ++i)
+            {
+                resultString[i] = numString[i];
+            }
+        }
+        else
+        {
+            strcpy(resultString, numString);
+        }
+    }
+    
+    
     // TODO: Convert string to int
-
+    int result;
+    if(flag2 == false)
+    {
+        result = 0 - convertStrToNum(resultString);
+    }
+    else
+    {
+        result = convertStrToNum(resultString);
+    }
     // TODO: Store in c pointer and return True
-
-    return false;
+    *c = result;
+    return true;
 }
 
 /*
@@ -65,17 +148,55 @@ bool mantissa(char numString[], int *numerator, int *denominator){
     // TODO: Obtain string of the mantissa
 
     // TODO: Convert string to int
-
+    int power = 1;
+    double result = 0;
+    int length = strlen(numString);
+    char *resultString;
+    resultString = (char*)malloc(length*sizeof(char));
+    bool flag = true;
+    int countFloat = 0;
+    for (int i = 0; i < length; ++i)
+    {
+        if (numString[i] == '.')
+        {
+            flag = false;
+            countFloat = i;
+        }
+    }
+    
+    if (flag == false)
+    {
+        for (int i = countFloat + 1; i < length; ++i)
+        {
+            resultString[i-(countFloat+1)] = numString[i];
+            power *= 10;
+        }
+    }
+    else
+    {
+        return false;
+    }
     // TODO: Construct denominator to be an int that is a power of
     //       10 that creates a proper float representing the
     //       mantissa and the numerator and denomiator are divided
 
     // TODO: Store numerator and denominator and return True
-
-    return false;
+    int template = convertStrToNum(resultString);
+    result = (double)template / power;
+    
+    *numerator = template;
+    *denominator = power;
+    return true;
 }
 
-// Maybe a general function combining characteristic() and
+// Maybe a general function combining characteristic() and 
 // mantissa(), implementing both to convert a string into int
-
+bool finalResult(char numString[], int * c, int *numerator, int *denominator)
+{
+    
+    bool result1 = characteristic(numString, c);
+    bool result2 = mantissa(numString, numerator, denominator);
+    printf("c is %d, numerator is %d, denominator is %d\n",*c, *numerator, *denominator );
+    return true;
+}
 #endif  // STRING_TO_INT_CONVERTER.C
